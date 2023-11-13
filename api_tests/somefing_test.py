b08f2f4dfd393d4cfc685d62fa9ae6eda9351c65
dboichenko1 <danilaups@yandex.ru>
2023-11-13 20:33:31 +0300

    test pycharm

--- api_tests/somefing_test.py
+++ api_tests/somefing_test.py
@@ -4,6 +4,6 @@ from basic_classes.response import Response
 from schemas.get_lib_export_data import GET_LIB_V1_CSHEMA, GET_LIB_V2_CSHEMA
 def test_one():
     v1 = requests.get(get_lib_export_data_v1)
-    # v2 = requests.get(get_lib_export_data_v2)
+    v2 = requests.get(get_lib_export_data_v2)
     Response(v1).assert_status_code(200).validate(GET_LIB_V1_CSHEMA)
-    # Response(v2).assert_status_code(200).validate(GET_LIB_V2_CSHEMA)
+    Response(v2).assert_status_code(200).validate(GET_LIB_V2_CSHEMA)
